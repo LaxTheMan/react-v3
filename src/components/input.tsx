97ca0faa39aa75e99e8adce6b3c635ae37336ccc
Laxman Prasad <lp1712002@gmail.com>
2024-11-12 10:43:15 +0900

    changed property style name

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,14 +1,14 @@
 type InputProps = {
   type: string;
   checked: boolean;
-  style?: string;
+  className?: string;
   onChange: (e: React.ChangeEvent<HTMLElement>) => void;
 };
 
-export const Input: React.FC<InputProps> = ({ type, checked, style = '', onChange }: InputProps) => {
+export const Input: React.FC<InputProps> = ({ type, checked, className = '', onChange }: InputProps) => {
   return (
     <div className="flex justify-between">
-      <input type={type} className={style} checked={checked} onChange={onChange} />
+      <input type={type} className={className} checked={checked} onChange={onChange} />
       {checked && <label className="mx-2">'hello world'</label>}
     </div>
   );
