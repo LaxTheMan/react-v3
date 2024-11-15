f6be4013c3cefaa3f5cb768ae652a8255d652e14
Laxman Prasad <lp1712002@gmail.com>
2024-11-15 17:23:47 +0900

    added attributes for Practice3 component

--- src/components/Practice3.tsx
+++ src/components/Practice3.tsx
@@ -1,5 +1,22 @@
+import { RegisterOptions, UseFormRegisterReturn } from 'react-hook-form';
+import { FormInput } from '../templates/Practice4';
 import { InputField } from './InputField';
 
 export const Practice3 = () => {
-  return <InputField />;
+  return (
+    <InputField
+      label={'Postal Code'}
+      register={function <
+        TFieldName extends 'Postal Code' | 'Prefecture' | 'City/Ward/Town' =
+          | 'Postal Code'
+          | 'Prefecture'
+          | 'City/Ward/Town',
+      >(
+        name: TFieldName,
+        options?: RegisterOptions<FormInput, TFieldName> | undefined,
+      ): UseFormRegisterReturn<TFieldName> {
+        throw new Error('Function not implemented.');
+      }}
+    />
+  );
 };
